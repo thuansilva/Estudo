c7cd5f90c2b7dee884237d125376a0fd6b15db56
Thuan <thuansilva14@gmail.com>
2020-11-18 19:05:00 -0300

    Trabalhando com Python

---
+++ Python/rabisco.py
@@ -0,0 +1,33 @@
+                        #Capitulo 1:
+
+# Imutabilidade: Novas strings criadas a partir de outras strings
+
+#    Uma String ja definida não pode ser mudada. Ex
+# Exemplo com error
+
+
+# minha_str = "livro python 3"
+# minha_str[13] = '2'
+# print(minha_str)
+
+
+# Exemplo sem error
+
+minha_str = "livro python 3"
+# print(minha_str[0:13])
+# minha_str = minha_str[0:13] +"2"
+# # print(minha_str)
+
+#Alguns Métodos
+
+print(minha_str.capitalize())
+print(minha_str.split(" ")) 
+print(minha_str.count("p")) 
+
+print("Aceita um numero {} e um {} no print".format(100,'texto'))
+print("Posso jogar um {:>20} para 20 pixel para a esqueda/direita".format('valor'))
+
+# Capitulo 3 - Manipulações básicas
+
+
+
