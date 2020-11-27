af39ef88c5ec769093573d1d7db2450eef572840
Thuan <thuansilva14@gmail.com>
2020-11-26 23:20:03 -0300

    código para extrair arquivos compactados

---
+++ Python/extrair_arquivos_zip.py
@@ -0,0 +1,15 @@
+import os 
+import zipfile
+import sys
+
+def main(path):
+    if not os.path.exists(path):
+        print("Arquivo {} não existe!".format(path))
+        sys.exit(-1)
+    else:
+        zfile=zipfile.ZipFile(path)
+        zfile.extractall()
+        print("Arquivos extraidos!")
+
+if __name__ == "__main__":
+    main("./saida.zip")
