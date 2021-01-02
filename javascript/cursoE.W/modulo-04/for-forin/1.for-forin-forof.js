713da69732bccd2b3e6d2b4798644f856507d33c
Thuan <thuansilva14@gmail.com>
2021-01-02 11:37:49 -0400

    ForIn e ForOf

--- javascript/cursoE.W/modulo-04/for-forin/1.for-forin-forof.js
+++ javascript/cursoE.W/modulo-04/for-forin/1.for-forin-forof.js
@@ -4,8 +4,26 @@ async function main() {
   try {
     const result = await server.abterPessoas();
     const names = [];
+    /*  
     for (let i = 0; i <= result.results.length - 1; i++) {
       let pessoa = result.results[i];
+      names.push(pessoa.name);
+    }
+    console.log("names", names);
+    */
+    // #########################################################################
+    // For in: cria uma variavel i, mas não precissa iniciar ela e passa o aray em questão
+    /* 
+    for (let i in result.results) {
+      let pessoa = result.results[i];
+      names.push(pessoa.name);
+    }
+    console.log("names", names);
+     */
+    // #########################################################################
+    // For of: ele ja mapeia o array em questão e coloca os dados na variavél
+
+    for (pessoa of result.results) {
       names.push(pessoa.name);
     }
     console.log("names", names);
