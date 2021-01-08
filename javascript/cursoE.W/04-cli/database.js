c6bd80f852e39558a59f2d5d6671fc5345edba34
Thuan <thuansilva14@gmail.com>
2021-01-08 16:35:26 -0400

    Manipulando arquivos - CREATE

--- javascript/cursoE.W/04-cli/database.js
+++ javascript/cursoE.W/04-cli/database.js
@@ -1,7 +1,8 @@
-const { readFile } = require("fs");
+const { readFile, writeFile } = require("fs");
 
 const { promisify } = require("util");
 const readFileAsync = promisify(readFile);
+const writeFileAsync = promisify(writeFile);
 
 class Database {
   constructor() {
@@ -11,7 +12,22 @@ class Database {
     const arquivo = await readFileAsync(this.NOME_ARQUIVO, "utf8");
     return JSON.parse(arquivo.toString());
   }
-  escreverDadosArquivo() {}
+  async escreverDadosArquivo(dados) {
+    await writeFileAsync(this.NOME_ARQUIVO, JSON.stringify(dados));
+    return true;
+  }
+  async cadastrar(heroi) {
+    const dados = await this.obterDadosArquivos();
+    const id = heroi.id <= 2 ? heroi.id : Date.now();
+
+    const heroiComID = {
+      id,
+      ...heroi,
+    };
+    const dadosFinal = [...dados, heroiComID];
+    const resultado = await this.escreverDadosArquivo(dadosFinal);
+    return resultado;
+  }
   async listar(id) {
     const dados = await this.obterDadosArquivos();
     const dadosFiltrados = dados.filter((item) => (id ? item.id === id : true));
--- javascript/cursoE.W/04-cli/test.js
+++ javascript/cursoE.W/04-cli/test.js
@@ -14,8 +14,10 @@ describe("Suite de manipulação de herois", () => {
     deepStrictEqual(resultado, expect);
   });
 
-  /*   it("Pesquisar um herois usando arquivos", async () => {
-    const Fexpect = DEFAULT_ITEM_CADASTRAR;
-    ok(null, expect);
-  }); */
+  it("Deve cadastrar um heroi, usando arquivos", async () => {
+    const expect = DEFAULT_ITEM_CADASTRAR;
+    const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+    const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
+    deepStrictEqual(actual, expect);
+  });
 });
