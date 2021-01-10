8143dbe497648beb17e121e4ff3b41132929566a
Thuan <thuansilva14@gmail.com>
2021-01-09 21:26:15 -0400

    Manipulando arquivos - DELETE

--- javascript/cursoE.W/04-cli/database.js
+++ javascript/cursoE.W/04-cli/database.js
@@ -33,6 +33,18 @@ class Database {
     const dadosFiltrados = dados.filter((item) => (id ? item.id === id : true));
     return dadosFiltrados;
   }
+  async remover(id) {
+    if (!id) {
+      return await escreverDadosArquivo([]);
+    }
+    const dados = await this.obterDadosArquivos();
+    const indice = dados.findIndex((item) => item.id === parseInt(id));
+    if (indice === -1) {
+      throw Error("O usuario não existe");
+    }
+    dados.splice(indice, 1);
+    return await this.escreverDadosArquivo(dados);
+  }
 }
 
 module.exports = new Database();
--- javascript/cursoE.W/04-cli/test.js
+++ javascript/cursoE.W/04-cli/test.js
@@ -1,4 +1,4 @@
-const { deepStrictEqual, ok } = require("assert");
+const { deepStrictEqual } = require("assert");
 const database = require("./database");
 
 const DEFAULT_ITEM_CADASTRAR = {
@@ -8,6 +8,9 @@ const DEFAULT_ITEM_CADASTRAR = {
 };
 
 describe("Suite de manipulação de herois", () => {
+  before(async () => {
+    await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
+  });
   it("Pesquisar um herois usando arquivos", async () => {
     const expect = DEFAULT_ITEM_CADASTRAR;
     const [resultado] = await database.listar(expect.id);
@@ -19,5 +22,11 @@ describe("Suite de manipulação de herois", () => {
     const resultado = await database.cadastrar(DEFAULT_ITEM_CADASTRAR);
     const [actual] = await database.listar(DEFAULT_ITEM_CADASTRAR.id);
     deepStrictEqual(actual, expect);
+  });
+
+  it("Deve remover um heroi", async () => {
+    const expect = true;
+    const resultado = await database.remover(DEFAULT_ITEM_CADASTRAR.id);
+    deepStrictEqual(resultado, expect);
   });
 });
