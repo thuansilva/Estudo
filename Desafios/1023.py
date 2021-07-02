185f200254314363a2e95c056aad6db1da2a3347
Thuan <thuansilva14@gmail.com>
2021-07-01 20:50:09 -0400

    Estudo do Desafio

---
+++ Desafios/1023.py
@@ -0,0 +1,29 @@
+''' 
+ A entrada deve conter um numero inteiro de N (1 ≤ N ≤ 1*106), ela indica a qunatidade de imoveis 
+
+ Informar um par de valores X e Y, 
+  X siginifica quantidade de pessoa na residencia e y o consumo total;
+ Nenhum Y é maior que 200;
+ O programa finaliza quando chega ao numero 0 
+
+ O calculo a ser feito será da seguinte forma 
+      Y/X, o resultado será um numero inteiro
+
+  Caso o numero inteiro ja tenha sido encontrado os 
+  moradores irão se somar e so vai ter 1 valor
+   exemplo: 20/2 = 10; 
+            31/3 = (int)10
+  
+  Posso trabalhar com uma tupla A=[[10,2],[10,3]]
+
+  A ordenação será feito por ordem ascendente de consumo;
+
+  Deve mostrar o consumo médio por pessoa da cidade, com 2 casas decimais sem arredondamento, 
+  considerando o consumo real total.
+
+  Imprimir uma linha em branco entre dois casos de teste consecutivos;
+
+  No fim da saída não deve haver uma linha em branco
+ '''
+
+ 
